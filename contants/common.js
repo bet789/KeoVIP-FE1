67e6415232197db2a9a5c0645c96d68398fd3381
Dee <deeit789@gmail.com>
2023-01-04 19:33:14 +0700

    Dee

--- contants/common.js
+++ contants/common.js
@@ -3,17 +3,24 @@ export const ADS_KEOVIP = "https://t.me/keovip4tv";
 export const URL_KEOVIP_ZALO = "https://zalo.me/0783208888";
 export const ADS_BANNER = "https://keovip.b-cdn.net/taipei-14-12.gif";
 export const ADS_BANNER_BOTTOM = "https://keovip.b-cdn.net/taipei-ads.gif";
-export const URL_789BET = "https://www.789betb.com/?uagt=livesbong1&path=signup";
-export const URL_NEW88 = "https://www.new88ww.com/?uagt=livesbong1&path=signup";
-export const URL_JUN88 = "https://www.jun88h.com/?uagt=livesbong1&path=signup";
-export const URL_VIDEO = "https://keovip.b-cdn.net/worldCup_2022.812323cc77452e7fc8171c31b25aad69.mp4";
-export const URL_IFRAME_THESPORTS = "https://stream.raysports.live/vi/football?token=72qdeh1l6hgu5rl";
-export const URL_IFRAME_CHAT = "https://a2zchat.com/embed/6348e17da24d2aea95abb068";
+export const URL_789BET =
+  "https://www.789betb.com/?uagt=livesbong1a02&path=signup";
+export const URL_NEW88 =
+  "https://www.new88k.com/?uagt=livesbong1a01&path=signup";
+export const URL_JUN88 =
+  "https://www.jun889.vip/?uagt=livesbong1a02&path=signup";
+export const URL_VIDEO =
+  "https://keovip.b-cdn.net/worldCup_2022.812323cc77452e7fc8171c31b25aad69.mp4";
+export const URL_IFRAME_THESPORTS =
+  "https://stream.raysports.live/vi/football?token=72qdeh1l6hgu5rl";
+export const URL_IFRAME_CHAT =
+  "https://a2zchat.com/embed/6348e17da24d2aea95abb068";
 export const URL_API_THESPORTS = "https://api.keovip.live/website/thesports";
 export const URL_GROUP_KEOVIP = "https://t.me/+iivt0xZFDhJjMmY1";
 export const URL_IMAGE_BACKGROUND = "https://keovip.b-cdn.net/login_bg@2x.webp";
 export const URL_FB_KEOVIP = "https://www.facebook.com/groups/keovip88";
-export const URL_AMINATION = "https://www.isportslive8.com/football/detail.html";
+export const URL_AMINATION =
+  "https://www.isportslive8.com/football/detail.html";
 export const URL_GIF_LIVE = "https://keovip.b-cdn.net/living.gif";
 export const URL_IFRAME_ODDS =
   "https://v05btsmart.fts368.com/?api=V05BT&zi=gd003i0054q4&c=VD&tp=darker&lang=vi-VN&code=V05BT&token=j8Ko5QhwFiopHRVRHKFzgTqQDg1BoiMmvibUR9W2Lvw";
